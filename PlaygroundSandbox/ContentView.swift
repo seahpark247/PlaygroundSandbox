b27f2c6a30523bd82450636828b70ee8bb38adeb
seahpark247 <famousjedo@gmail.com>
2025-03-08 16:46:58 -0600

    Practice - Paul's Swift Conference

--- PlaygroundSandbox/ContentView.swift
+++ PlaygroundSandbox/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by Seah Park on 3/8/25.
 //
 
+import ImagePlayground
 import SwiftUI
 
 struct ContentView: View {
+    @State private var description = "Dog in a party hat"
+    @State private var images = [Image]()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Type something", text: $description)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .writingToolsAffordanceVisibility(.visible)
+
+            Button("Make") {
+                Task(operation: makeImages)
+            }
+
+            ForEach(images.indices, id: \.self) {
+                images[$0]
+                    .resizable()
+                    .frame(width: 200, height: 200)
+            }
         }
-        .padding()
+    }
+
+    func makeImages() async {
+        do {
+            let creator = try await ImageCreator()
+
+            let results = creator.images(for: [.text(description)], style: .animation, limit: 4)
+
+            images.removeAll()
+
+            for try await result in results {
+                let cgImage = result.cgImage
+
+                let image = Image(cgImage, scale: 1, label: Text(description))
+                images.append(image)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
