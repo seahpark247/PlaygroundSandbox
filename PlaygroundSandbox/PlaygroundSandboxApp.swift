1bb1587b1346278dd55d36c7f441672671afaa19
seahpark247 <famousjedo@gmail.com>
2025-03-08 14:01:44 -0600

    Initial Commit

---
+++ PlaygroundSandbox/PlaygroundSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSandboxApp.swift
+//  PlaygroundSandbox
+//
+//  Created by Seah Park on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaygroundSandboxTests/PlaygroundSandboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSandboxTests.swift
+//  PlaygroundSandboxTests
+//
+//  Created by Seah Park on 3/8/25.
+//
+
+import Testing
+@testable import PlaygroundSandbox
+
+struct PlaygroundSandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
